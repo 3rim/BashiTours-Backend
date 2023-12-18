0cded58b9703ed03088de6fdda703afab81f1417
3rim <erim_medi@hotmail.de>
2023-12-18 16:27:08 +0100

    init commit

---
+++ src/main/java/com/bashitours/webapp/BashitoursApplication.java
@@ -0,0 +1,13 @@
+package com.bashitours.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BashitoursApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BashitoursApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bashitours/webapp/BashitoursApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bashitours.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BashitoursApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
